a6d2886a2436d02c1139a47fe97e8f1410f0d1b8
Nina Stefurak <nina.stefurak06@gmail.com>
2023-11-30 21:05:50 +0100

    react

---
+++ src/lib/firebase.js
@@ -0,0 +1,22 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// import the seed file
+
+const config = {
+    apiKey: 'AIzaSyDO7-jwITVu0zXhkiMvheg-8WcWWvXTkZE',
+    authDomain: 'picwave-app.firebaseapp.com',
+    projectId: 'picwave-app',
+    storageBucket: 'picwave-app.appspot.com',
+    messagingSenderId: '109812298213',
+    appId: '1:1098122982137:web:37e525ae70d82381d76828'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore
+
+// call the seed file once
+// seedDatabase(firebase);
+console.log('firebase', firebase);
+export { firebase, FieldValue };
