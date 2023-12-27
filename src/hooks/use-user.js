769cddbb5aa490aa5dbd02ff1bc2a728299a5558
Nina Stefurak <nina.stefurak06@gmail.com>
2023-12-27 21:47:35 +0100

    sidebar

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -3,11 +3,11 @@ import UserContext from '../context/user'
 import { getUserByUserId } from '../services/firebase';
 
 export default function useUser() {
-  const [activeUser, setActiveUser] = useState();
+  const [activeUser, setActiveUser] = useState({});
   const { user } = useContext(UserContext);
 
   useEffect(() => {
-    async function getUserObjByUserId(userId) {
+    async function getUserObjByUserId() {
        const response = await getUserByUserId(user.uid);
       setActiveUser(response);
     }
