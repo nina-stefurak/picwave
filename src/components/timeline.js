bdc8053db9844ed6acb02cea8c288d4b847d88da
Hlafira Sharsniova <Hlafira.Sharsniova@microsoft.wsei.edu.pl>
2024-01-05 17:09:05 +0100

    timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,23 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
+
 export default function Timeline() {
+    // нам нужно полуить фото юзера, который залогинился
+  const { photos } = usePhotos();
+
+    // при загрузке фото нам нужно использовать скелетон
+    // если у нас есть фото, покажите их
+    // если у юзера нет фоток, скажите ему их создать (парочку фото)
 return (
     <div className="container col-span-2">
-        <p>I am the timeline</p>
+        {!photos ? (
+               <Skeleton count={1} width={640} height={600} className='mb-5'/>
+        ) : photos?.length > 0 ? (
+            photos.map((content) => <Post key={content.docId} content={content}/>)
+        ) : (
+            <p className='text-center text-2xl'>Follow people to see photos</p> 
+        )}
     </div>
     );
 }
