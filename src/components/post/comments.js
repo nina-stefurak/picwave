e298a72943510b3ef8aaa8dbe2a83128c69d172a
DDiirreegg <arseniy.5444@gmail.com>
2024-01-07 01:36:02 +0100

    Fixed user authorization

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -11,7 +11,7 @@ export default function Comments({docId, comments: allComments, posted, commentI
       <div className="p-4 pt-1 pb-4">
         {comments.length >= 3 && (
         <p className="text-sm text-gray-base mb-1 cirsor-pointer">
-          View all {comments.length} comments
+          View all comments
         </p>
         )}
         {comments.slice(0, 3).map((item) => (
